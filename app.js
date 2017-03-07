923c45ba5db66152f15d08ffea7abfc10907a991
Anatoly Shumilov <shumilov-anato@yandex.ru>
2017-03-07 19:59:44 +0300

    fix

--- app.js
+++ app.js
@@ -1,18 +1,16 @@
 var TelegramBot = require('node-telegram-bot-api');
 var token = '331299898:AAFdMVnEqW2t4b5MqU0J861HRWj061WgbIU';
 var bot = new TelegramBot(token, {polling: true});
-var telegraf = require('telegraf');
-var app = new telegraf(process.env.token);
-
-app.command('start', (ctx) => {
-  console.log('start', ctx.from)
-ctx.reply('Welcome!')
-})
-
-app.command('start', function (ctx) {
-  console.log('start', ctx)
-ctx.reply('Welcome!')
-})
+var request = require('request');
 
 
-app.startPolling()
+
+bot.on('message', function (msg) {
+  var chatId = msg.chat.id;
+
+      return bot.sendMessage(chatId, "Hello! I am NodeJS bot! Soon I'll be ready", {caption: "I'm a bot!"});
+
+
+});
+
+
