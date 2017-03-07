a36472f7f9df404b51446ae3687e40493de43ba2
Anatoly Shumilov <shumilov-anato@yandex.ru>
2017-03-07 18:17:51 +0300

    update

--- app.js
+++ app.js
@@ -2,42 +2,19 @@ var TelegramBot = require('node-telegram-bot-api');
 var token = '331299898:AAFdMVnEqW2t4b5MqU0J861HRWj061WgbIU';
 var bot = new TelegramBot(token, {polling: true});
 
-var notes = [];
+bot.setMainMenuText(function (chat) { return "What do you want to do?"; });
 
-bot.on('message', function (msg) {
-  var chatId = msg.chat.id;
-  console.log(msg);
-  switch (msg.text){
-    case "Hello":
-      return bot.sendMessage(chatId, "Hello!", {caption: "I'm a bot!"});
-          break;
-    case "Привет!":
-      return bot.sendMessage(chatId, "Привет!", {caption: "I'm a bot!"});
-      break;
-    case "Привет":
-      return bot.sendMessage(chatId, "Привет Привет!", {caption: "I'm a bot!"});
-      break;
-    default:
-      return bot.sendMessage(chatId, "Hello! I am NodeJS bot! Soon I'll be ready", {caption: "I'm a bot!"});
-      break;
-  }
-});
-bot.onText(/\/напомни (.+) в (.+)/, function (msg, match) {
-  var userId = msg.from.id;
-  var text = match[1];
-  var time = match[2];
+bot.setMainMenuOptions(function (chat) {
+  return { "Hi": hiOption, "Bye": byeOption };
+};
 
-  notes.push( { 'uid':userId, 'time':time, 'text':text } );
+bot.onCommand("start", false, function (msg) { bot.sendMainMenu(msg.chat); })
 
-  bot.sendMessage(userId, 'Отлично! Я обязательно напомню, если буду жив :)');
-});
-setInterval(function(){
-  for (var i = 0; i < notes.length; i++){
-    var curDate = new Date().getHours() + ':' + new Date().getMinutes();
-    if ( notes[i]['time'] == curDate ) {
-      bot.sendMessage(notes[i]['uid'], 'Напоминаю, что вы должны: '+ notes[i]['text'] + ' сейчас.');
-      notes.splice(i,1);
-    }
-  }
-},1000);
+function hiOption(msg) {
+  bot.sendText(msg.chat, "Hi!");
+}
+
+function byeOption(msg) {
+  bot.sendText(msg.chat, "Bye!");
+}
 
