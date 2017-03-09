272f54388c49a056dd97346a0496da34399a0125
Anatoly Shumilov <shumilov-anato@yandex.ru>
2017-03-09 16:29:59 +0300

    add keyboard

--- app.js
+++ app.js
@@ -3,13 +3,14 @@ var token = '331299898:AAFdMVnEqW2t4b5MqU0J861HRWj061WgbIU';
 var bot = new TelegramBot(token, {polling: true});
 var request = require('request');
 
-
+var optionKeys = {
+    "parse_mode": "Markdown",
+    "reply_markup": {  "keyboard": [["Starships"],["Planets"]]  }
+}
 
 bot.on('message', function (msg) {
-  var chatId = msg.chat.id;
-
-      return bot.sendMessage(chatId, "Hello! I am NodeJS bot! Soon I'll be ready", {caption: "I'm a bot!"});
-
+    var chatId = msg.chat.id;
+    bot.sendMessage(chatId, "Hi! Do your choose:", optionKeys);
 
 });
 
